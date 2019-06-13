9e8fe88064e588eeb82d86378b6acba7247b23ba
Nick <nicholastysh@gmail.com>
2019-06-13 13:10:57 -0400

    animate/stillClicks/refreshDivOnBtnClick/ratings

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,17 @@
 
+//array for storing new buttons
 const gifName = ["cat", "dog", "fish", "bird", "plane"];
 
 function displayGifImage() {
-    
-    // variable for storing the "data-name" state of fixed_height_small_still.url 
+    // every time a button is clicked, empty the gif view area
+    $("#gif-view-area").empty();
+
+    // variable for storing the user input from button
     const searchResult = $(this).attr("data-name");
 
     //variable for giphy api that takes in the search data
-    const queryURL = "https://api.giphy.com/v1/gifs/search?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10&q=" + searchResult;
+    const apiKey = "0FbXW4F9HCcDpbAqGA9MlQ0zwpmzri6I";
+    const queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&limit=12&q=" + searchResult;
     
     //query the giphy api
     $.ajax({
@@ -17,43 +21,58 @@ function displayGifImage() {
         //loop through the JSON ten times and display a new <img> tag each time
         for (let i = 0; i < 10; i++) {
 
-            //create variable to reuse for animate and still URL's
+            //create reusable variables for animate and still URL's
             const stillImageUrl = response.data[i].images.fixed_height_small_still.url;
             const animateImageUrl = response.data[i].images.fixed_height_small.url;
-            console.log(queryURL)
-            console.log(stillImageUrl)
-            
-            /*  <img class="change-state" src="stillImageUrl" alt="gif image"
-                 data-still="stillImageURL" data-animate="animatedImageURL" data-state="still"> */
-            const gifImage = $("<img>");
+            const rating = response.data[i].rating
+            console.log(queryURL);
+            console.log(stillImageUrl);
+            console.log(rating)
+
+            /*  <div class="col-3"><img src="stillImageUrl" alt="gif image"  data-animate="animatedImageURL"
+                    data-still="stillImageURL" data-state="still" class="change-state"></div>*/
+             const div = $("<div>")       
+             div.addClass("col-3")
+
+
+            /*  <img src="stillImageUrl" alt="gif image"  data-animate="animatedImageURL"
+                    data-still="stillImageURL" data-state="still" class="change-state"> */       
+            const gifImage = $('<img>');
             gifImage.attr("src", stillImageUrl);
             gifImage.attr("alt", "gif image");
-            gifImage.attr("data-animate", animateImageUrl)
-            gifImage.attr("data-still", stillImageUrl)
-            gifImage.attr("data-state", "still")
-            gifImage.addClass("change-state")
+            gifImage.attr("data-animate", animateImageUrl);
+            gifImage.attr("data-still", stillImageUrl);
+            gifImage.attr("data-state", "still");
+            gifImage.addClass("change-state");
             //load gif ahead of of previous gif
-            $("#gif-view").prepend(gifImage);
+            $("#gif-view-area").prepend(gifImage);
+
+            const p = $("<span>");
+            p.text("Rating: " + rating)
+            $("#gif-view-area").prepend(p);
         }
     });
 }  
 
 function renderButtons() {
-    $("#buttons-view").empty();
+    $("#buttons-view-area").empty();
     for (let i = 0; i < gifName.length; i++) {
         //<button class="onGifClick" data-name="gifName[iterated]">text pulled from gifName[iterated]</button>
         const a = $("<button>");
         a.addClass("onGifClick");
+        // create attribute with user input data
         a.attr("data-name", gifName[i]);
+        // text for button
         a.text(gifName[i]);
-        $("#buttons-view").append(a);
+        // add new button to div where the buttons are kept
+        $("#buttons-view-area").append(a);
     }
 }
 
 
-$("#add-gif").on("click", function (event) {
+$("#add-gif-button").on("click", function (event) {
     event.preventDefault();
-    const gifNameFromText = $("#gif-input").val().trim();
+    const gifNameFromText = $("#gif-search-input").val().trim();
     gifName.push(gifNameFromText);
     console.log(gifName);
     renderButtons();
@@ -62,7 +81,7 @@ $("#add-gif").on("click", function (event) {
 $(document).on("click", ".onGifClick", displayGifImage);
 
 
-$(".change-state").on("click", function() {
+$(document).on("click", ".change-state", function() {
       // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
       var state = $(this).attr("data-state");
       // If the clicked image's state is still, update its src attribute to what its data-animate value is.
